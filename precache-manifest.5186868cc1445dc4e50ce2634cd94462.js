b7cc10f895562270829289e5481f5f164c6b1175
Angel Sulbaran <angeldv.sulbaran@gmail.com>
2020-06-12 16:34:22 +0200

    Updates

---
+++ precache-manifest.5186868cc1445dc4e50ce2634cd94462.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa03326d7a4a1974f9f8c8e23a49c106",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "cd8eea713dff572ba11f",
+    "url": "/weather-app/static/css/main.b1cbfa5b.chunk.css"
+  },
+  {
+    "revision": "6f9da207b79e184da8ff",
+    "url": "/weather-app/static/js/2.095184d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.095184d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd8eea713dff572ba11f",
+    "url": "/weather-app/static/js/main.05e7416e.chunk.js"
+  },
+  {
+    "revision": "12ef64cf8e1aad5a2dd2",
+    "url": "/weather-app/static/js/runtime-main.c90f5b59.js"
+  },
+  {
+    "revision": "4a4bf872953192f0efc57512b02dcf92",
+    "url": "/weather-app/static/media/clear-wall.4a4bf872.jpg"
+  },
+  {
+    "revision": "164c68713b6afec6c43c3614f629613b",
+    "url": "/weather-app/static/media/cloudy-wall.164c6871.jpg"
+  },
+  {
+    "revision": "1d376cf485f19dc5e6e32bad1e06b066",
+    "url": "/weather-app/static/media/cold-wall.1d376cf4.jpg"
+  },
+  {
+    "revision": "5e35a549bc23d3fbb498021246daed8a",
+    "url": "/weather-app/static/media/rainy-wall.5e35a549.jpg"
+  },
+  {
+    "revision": "f867bf2245cc81ad717b473a6d144a0f",
+    "url": "/weather-app/static/media/wall.f867bf22.jpg"
+  }
+]);
